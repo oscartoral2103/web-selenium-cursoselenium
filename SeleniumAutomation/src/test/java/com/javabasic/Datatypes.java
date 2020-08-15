4825702849bd5d1d205f3d2d5d9068707149f0cb
oscartoral2103 <56484660+oscartoral2103@users.noreply.github.com>
2020-08-15 12:01:43 -0500

    Archivos de Java

---
+++ SeleniumAutomation/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,62 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+*DATOS
+*/
+
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		
+//		x=127;
+//		y=32767;
+//		z=2147483647;
+//		a=9223372036854775807L;
+		
+//		System.out.println(x);
+
+		
+		/*
+		*DATOS NUMERICOS REALES
+		*/		
+		
+//		float base;
+//		double altura;
+//	
+//	   base=30.5F;
+//	   altura=70.8;
+//	   System.out.println(base);
+//	   System.out.println(altura);
+//	   
+	   
+		
+//		char letra1;
+//		char letra2;
+//		String frase;
+//		
+//		letra1 ='a';
+//		letra2 ='\u0047';
+//		frase= "Que onda";
+//		
+//		System.out.println(letra2);
+//		System.out.println(frase);
+//		
+//		
+	
+		boolean avesVuelan;
+		avesVuelan = true;
+		System.out.println("Las Aves Vuelan?"+ avesVuelan);
+	
+	}
+	
+		
+	}
+
+
+
---
+++ SeleniumAutomation/src/test/java/com/javabasic/OperatorsStatements.java
@@ -0,0 +1,65 @@
+package com.javabasic;
+
+public class OperatorsStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+Operadores Aritmeticos
+*/
+
+//		int a=5;
+//		int b=20;
+//		int suma;
+//		
+//		suma =a+b;
+//		
+//		System.out.println(suma);
+		
+		/*
+		 * Operadores Unarios
+		 */
+//		
+//		int valor1 =3;
+//		int incrementar =++valor1;
+//		System.out.println(incrementar);
+		
+		/*
+		 * Operadores Relacionales
+		 */
+
+//	int num1 =4;
+//	int num2 =4;
+//	
+//	if(num1 != num2) {
+//		System.out.println("Los numeros son iguales");
+//		
+//	}else {
+//			System.out.println("Nel no son iguales");	
+//		}
+//	
+//	
+		
+		/*
+		 * 
+		 */
+		
+	int num1 =10;
+	int num2 =4;
+
+		if(num1 >=10 && num2 >10) {
+		System.out.println("Los numeros son mayores a 10");
+			
+		}else {
+				System.out.println("Nel los numeros no son iguales");	
+			}
+
+
+		
+		
+		
+		
+	}
+	}
+
+
