6a611f5caa2f18732a829c8f5458dfecd6ac365b
oscartoral2103 <56484660+oscartoral2103@users.noreply.github.com>
2020-08-28 20:03:59 -0500

    Aqquqi practicando

--- SeleniumAutomation/src/test/java/com/javabasic/Udemy.java
+++ SeleniumAutomation/src/test/java/com/javabasic/Udemy.java
@@ -1,5 +1,7 @@
 package com.javabasic;
 
+import java.util.Scanner;
+
 public class Udemy {
 
 	public static void main(String args[]) {
@@ -20,20 +22,36 @@ public class Udemy {
 //		System.out.println(VariableCadena);
 //		
 //		
-
-		String usuario = "Juan";
-		String titulo = "Ingeniero";
-		String Union = titulo + " " + usuario;
-		System.out.println("Union = " + Union);
-
-		int Num1 = 15;
-		int Num2 =10;
-		System.out.println(Num1 + Num2);//Se realiza la suma de Numeros
-		
-		System.out.println(Num1 + Num2 + usuario);//Evaluacion de izquierda a derecha realiza suma
-		System.out.println(usuario + (Num2 + Num1));//Se realiza la suma por el parentesisi, sin este, pinta los valores de Num1 y Num2
-		
-		
+//
+//		String usuario = "Juan";
+//		String titulo = "Ingeniero";
+//		String Union = titulo + " " + usuario;
+//		System.out.println("Union = " + Union);
+//
+//		int Num1 = 15;
+//		int Num2 =10;
+//		System.out.println(Num1 + Num2);//Se realiza la suma de Numeros
+//		
+//		System.out.println(Num1 + Num2 + usuario);//Evaluacion de izquierda a derecha realiza suma
+//		System.out.println(usuario + (Num2 + Num1));//Se realiza la suma por el parentesisi, sin este, pinta los valores de Num1 y Num2
+//		
+//		
+//		
+//		String Nombre = "karla";
+//		System.out.println("Nueva Linea: \n" + Nombre);//SALTO DE LINEA
+//		System.out.println("Tabulador: \t" + Nombre);//UN TABULADOR--ESPACIO
+//		System.out.println("Retroceso: \b\b" + Nombre); //Retrocceso
+//		System.out.println("Comilla Simple: \'" + Nombre + "'");//Comilla Simple
+//		System.out.println("Comilla Doble: \"" + Nombre +"\"");//Comilla Doble
+//		
+//		
+		System.out.println("Escribe tu nombre");
+		Scanner Consola = new Scanner(System.in);
+		String Usuario =Consola.nextLine();
+				System.out.println("Usuario = " + Usuario);
+				System.out.println("Escribe el Titulo");
+				String Titulo =Consola.nextLine();
+				System.out.println("Resultado: " + Titulo + " " + Usuario);
 		
 		
 		
